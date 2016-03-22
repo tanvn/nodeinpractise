50168f7398caab56c27aabaf6f81c8f141f5c043
t-vu <t-vu@dimage.co.jp>
2016-03-22 18:50:15 +0900

    update techs

--- chapter2/tech3.js
+++ chapter2/tech3.js
@@ -6,3 +6,18 @@ var obj = require("./techique-3");
 
 obj.one();
 obj.two();
+
+console.log(delete require.cache[require.resolve('./techique-3')]);
+obj.one();
+console.log(require.extensions)
+console.log('__dirname:', __dirname);
+console.log('__filename:', __filename);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(text) {
+	process.stdout.write(text.toUpperCase());
+});
+var user = { name: 'alex' };
+console.log('Hello:', user);
+console.error('Error, bad user:', user);
+console.trace()
