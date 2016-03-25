5fba06967cc8e947ac77152b882b69d4d2942da5
t-vu <t-vu@dimage.co.jp>
2016-03-25 19:11:29 +0900

    tech14 updated

--- chapter2/tech14.js
+++ chapter2/tech14.js
@@ -12,3 +12,31 @@ function complexOperations() {
 complexOperations().on('success', function() {
 	console.log('success!');
 });
+
+var EventEmitter = require('events').EventEmitter;
+var fs = require('fs');
+var content;
+function readFileIfRequired(cb) {
+	if (!content) {
+		fs.readFile(__filename, 'utf8', function(err, data) {
+			content = data;
+			console.log('readFileIfRequired: readFile');
+			cb(err, content);
+		});
+	} else {
+//		console.log('readFileIfRequired: cached');
+//		cb(null, content);
+		process.nextTick(function() {
+			console.log('readFileIfRequired: cached');
+			cb(null, content);
+		});
+	}
+}
+readFileIfRequired(function(err, data) {
+	console.log('1. Length:', data.length);
+	readFileIfRequired(function(err, data2) {
+		console.log('2. Length:', data2.length);
+	});
+	console.log('Reading file again...');
+});
+console.log('Reading file...');
